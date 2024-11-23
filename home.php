f20c68d31a4612aea84fedc149fd1a1b1ac87f81
hamadat <hamadat@MacBook-Pro-de-hamadat.local>
2024-11-23 10:38:12 +0100

    add navbar and menu

--- home.php
+++ home.php
@@ -4,8 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+
+    <!-- Swipe css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- Font awesome cdn link -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+
+   <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
+
+<!-- Header section starts -->
+
+<section class="header">
+    <a href="home.php" class="logo">travel.</a>
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+        
+
+</section>
+
+<!-- Header section end -->
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Swipe js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+ <script src="script.js"></script>
 </body>
 </html>
